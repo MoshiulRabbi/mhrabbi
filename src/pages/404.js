b775f2afa1a4d07062f49ac7d789af34500021cd
unknown <msmfd.com>
2020-10-14 15:25:04 +0600

    updatelogod404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,10 +4,16 @@ import { Layout, SEO } from "../components/common"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
+  <center>
+    <br></br><br></br><br></br><br></br><br></br><br></br>
     <h1>NOT FOUND</h1>
+    <br></br><br></br>
     <p>OOPS! Sorry!...</p>
-  </Layout>
+    <br></br>
+    <h3>Will Be Added Soon</h3>
+    <br></br><br></br><br></br><br></br>
+    </center>
+    </Layout>  
 )
 
 export default NotFoundPage
